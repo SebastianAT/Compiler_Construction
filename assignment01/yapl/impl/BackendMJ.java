0af993887faa5791d6c1c6666861f4d61bf288f1
Michael Eichholzer <mieichholzer@edu.aau.at>
2020-03-27 18:27:48 +0100

    Implemented basics for static data, added information to some of the methods

--- assignment01/yapl/impl/BackendMJ.java
+++ assignment01/yapl/impl/BackendMJ.java
@@ -1,18 +1,29 @@
 package yapl.impl;
 
 import yapl.interfaces.*;
+
 import java.io.IOException;
 import java.io.OutputStream;
+import java.nio.ByteBuffer;
+import java.util.ArrayList;
+import java.util.List;
 
 
 public class BackendMJ implements BackendBinSM {
+
+    private int pc = 0; //ProgrammCount
+    private int sc = 0; //StaticCount
+    private List<byte> sb = new ArrayList<byte>(); //StaticDataBytes
+
+
     @Override
     public int wordSize() {
-        return 0;
+        return 0 /*System.getProperty("sun.arch.data.model")*/;
     }
 
     @Override
     public int boolValue(boolean value) {
+        if (value) return 1;
         return 0;
     }
 
@@ -28,22 +39,28 @@ public class BackendMJ implements BackendBinSM {
 
     @Override
     public int allocStaticData(int words) {
-        return 0;
+        sc += words;
+        return sc - words;
     }
 
     @Override
     public int allocStringConstant(String string) {
-        return 0;
+        int addr = allocStaticData(string.length() + 1);
+        byte[] sbytes = string.getBytes();
+        for(byte b:sbytes) sb.add(b);
+        sb.add((byte) 0);
+        return addr;
     }
 
     @Override
     public int allocStack(int words) {
+        //
         return 0;
     }
 
     @Override
     public void allocHeap(int words) {
-
+        //0x1F XX XX: allocate space on heap with size XX XX
     }
 
     @Override
@@ -53,22 +70,24 @@ public class BackendMJ implements BackendBinSM {
 
     @Override
     public void allocArray() {
-
+        //0x20
     }
 
     @Override
     public void loadConst(int value) {
-
+        //0x0F - 0x14 : Const 0-5
+        //0x15 : -1
+        //0x16 XX XX XX XX : Cont with value XX XX XX XX
     }
 
     @Override
     public void loadWord(MemoryRegion region, int offset) {
-
+        //0x21
     }
 
     @Override
     public void storeWord(MemoryRegion region, int offset) {
-
+        //0x22
     }
 
     @Override
@@ -98,42 +117,42 @@ public class BackendMJ implements BackendBinSM {
 
     @Override
     public void neg() {
-
+        //0x1C
     }
 
     @Override
     public void add() {
-
+        //0x17
     }
 
     @Override
     public void sub() {
-
+        //0x18
     }
 
     @Override
     public void mul() {
-
+        //0x19
     }
 
     @Override
     public void div() {
-
+        //0x1A
     }
 
     @Override
     public void mod() {
-
+        //0x1B
     }
 
     @Override
     public void and() {
-
+        //Idee: Zuerst add und dann shift right
     }
 
     @Override
     public void or() {
-
+        // Geht das umsetzen ohne IF?
     }
 
     @Override
